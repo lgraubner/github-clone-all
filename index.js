e2fb6da0aaa09b4c8f897b6ede2826420f78d029
Lars Graubner <mail@larsgraubner.de>
2019-08-31 20:00:02 +0200

    fix repo count, fix ignore-forks, improve structure

--- index.js
+++ index.js
@@ -11,17 +11,11 @@ const fs = require('fs')
 
 const pkg = require('./package.json')
 
-const args = process.argv.slice(2)
+function logError(...messages) {
+  console.error(`${chalk.red('Error!')} ${messages.join('\n')}`)
+}
 
-const options = mri(args, {
-  default: {
-    'ignore-forks': false,
-    ignore: '',
-    'max-concurrency': 5
-  }
-})
-
-if (options.help) {
+function help() {
   console.log(`
   Usage: github-clone-all [options] [destination]
 
@@ -36,37 +30,19 @@ if (options.help) {
     --ignore <repos>          comma seperated list of repositories to ignore
     --max-concurrency <num>   max concurrent clone processes (default: 5)
   `)
-  process.exit()
 }
 
-if (options.version) {
-  console.log(pkg.version)
-  process.exit()
-}
-
-if (!options['access-token']) {
-  throw new Error('Github access token is required!')
-}
-
-if (!options.username) {
-  throw new Error('Github username is required')
-}
-
-const ignoredRepos = options.ignore.split(',')
-
-let done = 0
-
-function fetchRepositories() {
+function fetchRepositories(username, accessToken) {
   return axios({
     url: 'https://api.github.com/graphql',
     method: 'post',
     headers: {
-      Authorization: `bearer ${options['access-token']}`
+      Authorization: `bearer ${accessToken}`
     },
     data: {
       query: `
           query {
-              user(login: "${options.username}") {
+              user(login: "${username}") {
                   repositories(first: 100) {
                       edges {
                           node {
@@ -77,7 +53,7 @@ function fetchRepositories() {
                       }
                   }
               }
-              organization(login: "${options.username}") {
+              organization(login: "${username}") {
                   repositories(first: 100) {
                       edges {
                           node {
@@ -94,7 +70,7 @@ function fetchRepositories() {
   })
 }
 
-function download(repository, dest, spinner, callback) {
+function download(repository, accessToken, dest, spinner, callback) {
   const { url, name } = repository
 
   const filePath = path.resolve(dest, `${name}.tar.gz`)
@@ -102,9 +78,7 @@ function download(repository, dest, spinner, callback) {
   const exists = fs.existsSync(filePath)
 
   if (!exists) {
-    const curl = `curl -H "Authorization: token ${
-      options['access-token']
-    }" -L ${url}/tarball/master > ${filePath}`
+    const curl = `curl -H "Authorization: token ${accessToken}" -L ${url}/tarball/master > ${filePath}`
 
     exec(curl, function(err) {
       if (err) {
@@ -116,62 +90,136 @@ function download(repository, dest, spinner, callback) {
   } else {
     callback(null)
   }
-
-  done++
 }
 
-fetchRepositories().then(res => {
-  if (res.data.data && Object.keys(res.data.data).length != 0) {
-    const repos = (res.data.data.user || res.data.data.organization)
-      .repositories.edges
+async function main(argv_) {
+  const args = argv_.slice(2)
 
-    let dest = './'
-    if (options._.length) {
-      // eslint-disable-next-line
-      dest = options._[0]
+  const options = mri(args, {
+    default: {
+      'ignore-forks': false,
+      ignore: '',
+      'max-concurrency': 5
     }
+  })
 
-    const destFolder = path.resolve(dest)
+  if (options.help) {
+    help()
+    process.exit()
+  }
 
-    // create target folder if not exists
-    if (!fs.existsSync(destFolder)) {
-      fs.mkdirSync(destFolder)
-    }
+  if (options.version) {
+    console.log(pkg.version)
+    process.exit()
+  }
 
-    const spinner = ora(`Downloading archives...`).start()
+  if (!options['access-token']) {
+    logError(
+      'A Github access token is required.',
+      'Please go to https://github.com/settings/tokens/new and issue a token with all repo scopes enabled.'
+    )
 
-    eachLimit(
-      repos,
-      options['max-concurrency'],
-      (repo, callback) => {
-        const { name, isFork, node } = repo
-        if (
-          ignoredRepos.indexOf(name) !== -1 ||
-          (options['ignore-forks'] && isFork)
-        ) {
-          callback(null)
-          return
+    return 1
+  }
+
+  if (!options.username) {
+    logError('Please provide a Github username.')
+
+    return 1
+  }
+
+  const ignoredRepos = options.ignore.split(',')
+
+  let done = 0
+
+  try {
+    const res = await fetchRepositories(
+      options.username,
+      options['access-token']
+    )
+
+    if (res.data.data && Object.keys(res.data.data).length != 0) {
+      const userOrOrg = res.data.data.user || res.data.data.organization
+
+      if (userOrOrg === null) {
+        logError('User or organisation not found.')
+        return 1
+      }
+
+      const repos = userOrOrg.repositories.edges
+
+      let dest = './'
+      if (options._.length) {
+        // eslint-disable-next-line
+        dest = options._[0]
+      }
+
+      const destFolder = path.resolve(dest)
+
+      // create target folder if not exists
+      if (!fs.existsSync(destFolder)) {
+        fs.mkdirSync(destFolder)
+      }
+
+      const spinner = ora(`Downloading archives...`).start()
+      const repoCount = repos.reduce((count, repo) => {
+        if (options['ignore-forks'] && repo.node.isFork) {
+          return count
         }
 
-        spinner.text = `Downloading archives... (${done}/${
-          repos.length
-        }) ${chalk.dim(node.name)}`
+        count++
 
-        download(node, dest, spinner, callback)
-      },
-      () => {
-        spinner.succeed(
-          `Downloaded repositories for Github user ${
-            options.username
-          } to "${path.resolve(dest)}".`
-        )
-        process.exit()
-      }
-    )
-    return
+        return count
+      }, 0)
+
+      eachLimit(
+        repos,
+        options['max-concurrency'],
+        (repo, callback) => {
+          const { name, isFork } = repo.node
+          if (
+            ignoredRepos.indexOf(name) !== -1 ||
+            (options['ignore-forks'] && isFork)
+          ) {
+            callback(null)
+            return
+          }
+
+          spinner.text = `Downloading archives... (${done}/${repoCount}) ${chalk.dim(
+            name
+          )}`
+
+          download(repo.node, options['access-token'], dest, spinner, () => {
+            done++
+            callback()
+          })
+        },
+        () => {
+          spinner.succeed(
+            `Downloaded repositories for Github user ${
+              options.username
+            } to "${path.resolve(dest)}".`
+          )
+          process.exit()
+        }
+      )
+      return
+    }
+    if (res.data.errors && res.data.errors.length) {
+      res.data.errors.map(e => e.message).map(console.error)
+      return
+    }
+  } catch (err) {
+    console.log(err)
+    if (err.response.status === 401) {
+      logError(
+        'Your access token is invalid',
+        'Please go to https://github.com/settings/tokens/new and issue a token with all repo scopes enabled.'
+      )
+    } else {
+      throw err
+    }
   }
-  if (res.data.errors && res.data.errors.length) {
-    res.data.errors.map(e => e.message).map(console.error)
-    return
-  }
-})
+}
+
+main(process.argv)
